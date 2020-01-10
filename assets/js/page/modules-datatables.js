4ab27e6eda6edbe895e95f0bf9b16c1250747a01
shensong <2269429539@qq.com>
2020-01-10 23:42:35 +0800

    阅读页面格式替换

--- assets/js/page/modules-datatables.js
+++ assets/js/page/modules-datatables.js
@@ -31,8 +31,31 @@ $("[data-checkboxes]").each(function() {
 $("#table-1").dataTable({
   "columnDefs": [
     { "sortable": false, "targets": [2,3] }
+  ],
+  "oLanguage": {
+    "sLengthMenu": "每页显示 _MENU_ 条记录",
+    "sZeroRecords": "抱歉， 没有找到",
+    "sInfo": "从 _START_ 到 _END_ /共 _TOTAL_ 条数据",
+    "sInfoEmpty": "没有数据",
+    "sInfoFiltered": "(从 _MAX_ 条数据中检索)",
+    "oPaginate": {
+    "sFirst": "首页",
+    "sPrevious": "前一页",
+    "sNext": "后一页",
+    "sLast": "尾页"
+    }
+  },
+  "sZeroRecords": "没有检索到数据",
+  "bFilter": true, //过滤功能
+  "bSort": false, //排序功能
+  "aLengthMenu": [2, 4, 6, 8],
+  "aoColumns": [
+    { "asSorting": [ " desc" , " asc" ] },
+    { "asSorting": [ ] },
+    { "asSorting": [ ] }
   ]
 });
+
 $("#table-2").dataTable({
   "columnDefs": [
     { "sortable": false, "targets": [0,2,3] }
